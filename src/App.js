2d598e08147b55d2edba26afc01fd890b4e01bfb
mziyabo <mziyabo@live.com>
2024-04-11 19:01:29 +0200

    test: add app.js

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import './App.scss';
+import DataGrid from './components/grid/vulnerability-report';
+import Dashboard from './components/dashboard/dashboard';
+import { Tab, Tabs } from "@mui/material";
+import React, { useState } from 'react';
+
+function App() {
+
+  // TODO: CLEANUP - for local testing only
+  const url = "http://localhost:8000/vulnreport.raw.json"
+
+  const [currentTabIndex, setCurrentTabIndex] = useState(0);
+
+  const handleTabChange = (e, tabIndex) => {
+    setCurrentTabIndex(tabIndex);
+  };
+
+  return (
+    <React.Fragment>
+      <Tabs value={currentTabIndex} onChange={handleTabChange}>
+        <Tab label='Table'/>
+        <Tab label='Dashboard'/>
+      </Tabs>
+      {currentTabIndex === 0 && (
+        <DataGrid reportUrl={url} />
+      )}
+      {currentTabIndex === 1 && (
+        <Dashboard reportUrl={url} />
+      )}
+    </React.Fragment>
+  );
+}
+
+export default App;
