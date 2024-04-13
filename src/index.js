79839a803cdfd1c4a2319ce678b8f810d34a510e
mziyabo <mziyabo@live.com>
2024-04-13 09:49:13 +0200

    feat: multi-container support

--- src/index.js
+++ src/index.js
@@ -1,56 +1,55 @@
 import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
 import './index.css';
 import { Tab, Tabs } from "@mui/material";
 import DataGrid from './components/grid/vulnerability-report';
 import Dashboard from './components/dashboard/dashboard';
 
-// TODO: remove this for production - only required testing locally
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
-
 const Extension = (props) => {
 
   const { resource, application } = props;
   const appName = application?.metadata?.name || "";
-  let container = resource?.spec?.template?.spec?.containers[0]?.name || "";
-  let resourceName = resource?.metadata?.name || "";
-  let resourceNamespace = resource?.metadata?.namespace || "";
-  let resourceKind = resource?.kind?.toLowerCase() || "";
+  const resourceNamespace = resource?.metadata?.namespace || "";
+  const isPod = resource?.kind === "Pod"
+  const resourceName = isPod ? resource?.metadata?.ownerReferences[0].name.toLowerCase() : resource?.metadata?.name;
+  const resourceKind = isPod ? resource?.metadata?.ownerReferences[0].kind.toLowerCase() : resource?.kind?.toLowerCase();
 
+  let [container] = useState(isPod ? resource?.spec?.containers[0]?.name : resource?.spec?.template?.spec?.containers[0]?.name);
 
-  if (resource?.kind === "Pod") {
-    container = resource?.spec?.containers[0]?.name
-    resourceName = resource?.metadata?.ownerReferences[0].name.toLowerCase()
-    resourceKind = resource?.metadata?.ownerReferences[0].kind.toLowerCase()
-  }
+  const baseURI = `${window.location.origin}/api/v1/applications/${appName}/resource`
+  let [reportUrl, setReportUrl] = useState(`${baseURI}?name=${resourceKind}-${resourceName}-${container}&namespace=${resourceNamespace}&resourceName=${resourceKind}-${resourceName}-${container}&version=v1alpha1&kind=VulnerabilityReport&group=aquasecurity.github.io`);
 
-  const reportUrl = `${window.location.origin}/api/v1/applications/${appName}/resource?name=${resourceKind}-${resourceName}-${container}&namespace=${resourceNamespace}&resourceName=${resourceKind}-${resourceName}-${container}&version=v1alpha1&kind=VulnerabilityReport&group=aquasecurity.github.io`
+  const containers = isPod ? resource?.spec?.containers.map(c => c.name) : resource?.spec?.template?.spec?.containers.map(c => c.name)
 
   const [currentTabIndex, setCurrentTabIndex] = useState(0);
   const handleTabChange = (e, tabIndex) => {
-    console.log(tabIndex);
     setCurrentTabIndex(tabIndex);
   };
 
+  const onOptionChangeHandler = (event) => {
+    container = event.target.value
+    setReportUrl(`${baseURI}?name=${resourceKind}-${resourceName}-${container}&namespace=${resourceNamespace}&resourceName=${resourceKind}-${resourceName}-${container}&version=v1alpha1&kind=VulnerabilityReport&group=aquasecurity.github.io`)
+  };
+
   return (
-    <React.Fragment>
-      <Tabs value={currentTabIndex} onChange={handleTabChange}>
-        <Tab label='Table' />
-        <Tab label='Dashboard' />
-      </Tabs>
-      {currentTabIndex === 0 && (
-        <DataGrid reportUrl={reportUrl} />
-      )}
-      {currentTabIndex === 1 && (
-        <Dashboard reportUrl={reportUrl} />
-      )}
-    </React.Fragment>
+    <div>
+      <React.Fragment>
+        <select class="vulnerability-report__container_dropdown" onChange={onOptionChangeHandler}>
+          {containers.map((container, index) => {
+            return (<option key={index} value={container}>{container}</option>)
+          })}
+        </select>
+        <Tabs value={currentTabIndex} onChange={handleTabChange}>
+          <Tab label='Table' />
+          <Tab label='Dashboard' />
+        </Tabs>
+        {currentTabIndex === 0 && (
+          <DataGrid reportUrl={reportUrl} />
+        )}
+        {currentTabIndex === 1 && (
+          <Dashboard reportUrl={reportUrl} />
+        )}
+      </React.Fragment>
+    </div>
   );
 };
 
